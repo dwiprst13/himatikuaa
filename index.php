f627c7520b326c33dcc53d8bad0ebe30d28e9331
dwiprst13 <dwiprst13@gmail.com>
2024-05-26 09:18:43 +0700

    yekk

--- index.php
+++ index.php
@@ -5,10 +5,10 @@ session_start();
 if (isset($_SESSION['role']) && $_SESSION['role'] == 'admin') {
     $title = "HIMATIK - UAA - Admin Page";
     include("templates/head.php");
-    include("controller/admin_page.php");
+    include("routes/admin_page.php");
 } else {
     $title = "HIMATIK - UAA";
     include("templates/head.php");
-    include("controller/user_page.php");
+    include("routes/user_page.php");
 }
 ?>
